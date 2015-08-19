90649207288603dc718ed9c72cc1a4b86f2ca559
tamzi <tamrefrank@gmail.com>
2015-08-19 14:05:47 +0300

    JS initialized

--- beMyValentine/js/script.js
+++ beMyValentine/js/script.js
@@ -1,1 +1,11 @@
         /*Your Custom Javascript file goes here.*/
+$(document).ready(function(){
+  $('.title').click(function(){
+    $('.container').addClass('open');
+  });
+
+
+  $('.close').click(function(){
+    $('.container').removeClass('open');
+  });
+});
