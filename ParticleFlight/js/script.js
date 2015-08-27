a5def5d7360839dc43800d992e98bbe9d0d1a08b
tamzi <tamrefrank@gmail.com>
2015-08-27 10:55:14 +0300

    starting the js file

--- ParticleFlight/js/script.js
+++ ParticleFlight/js/script.js
@@ -1,1 +1,12 @@
         /*Your Custom Javascript file goes here.*/
+
+
+var $c = $(".c"),
+		$wrap = $(".wrap"),
+		w = window.innerWidth,
+		h = window.innerHeight;
+
+function random(min, max){
+	return (Math.random() * (max - min)) + min;
+}
+
