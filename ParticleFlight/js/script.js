a65229617aad4b8b571424e85e61f91a1e12b6fa
tamzi <tamrefrank@gmail.com>
2015-08-27 10:55:51 +0300

    setting backGround color

--- ParticleFlight/js/script.js
+++ ParticleFlight/js/script.js
@@ -9,4 +9,20 @@ var $c = $(".c"),
 function random(min, max){
 	return (Math.random() * (max - min)) + min;
 }
+$c.each(function(i){
+
+	var x = random(0, w),
+			y = random(0, h),
+			z = random(-1000, -200),
+			color = "hsla("+i * 1.8+", 50%, 50%, 1)",
+			size = random(2, 30),
+			c = $(this);
+
+	c.css({
+		background: color,
+		height: size,
+		width: size,
+		borderRadius: "50%",
+		boxShadow: "0 0 "+size+"px " + color
+	})
 
