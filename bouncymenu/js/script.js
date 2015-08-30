cafe5ed48d096c2c47572b5f22e1d2a519872ca7
tamzi <tamrefrank@gmail.com>
2015-08-30 06:55:06 +0300

    assigning each function to the css stages of the menu

--- bouncymenu/js/script.js
+++ bouncymenu/js/script.js
@@ -6,5 +6,16 @@ $(document).ready(function(){
 	var startingAngle=180+(-angle/2);
 	var slice=angle/(menuItemNum);
 	TweenMax.globalTimeScale(0.8);
+	$(".menu-item").each(function(i){
+		var angle=startingAngle+(slice*i);
+		$(this).css({
+			transform:"rotate("+(angle)+"deg)"
+		})
+		$(this).find(".menu-item-icon").css({
+			transform:"rotate("+(-angle)+"deg)"
+		})
+	})
+	var on=false;
+
 
 });
