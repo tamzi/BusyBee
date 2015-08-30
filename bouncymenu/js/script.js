97471294892a08adcd8a1fe3384b2e5aef9105d6
tamzi <tamrefrank@gmail.com>
2015-08-30 06:59:19 +0300

    function open and close menu

--- bouncymenu/js/script.js
+++ bouncymenu/js/script.js
@@ -49,6 +49,92 @@ $(document).on("touchend",function(){
 		on?openMenu():closeMenu();
 
 	}
+function openMenu(){
+		$(".menu-item").each(function(i){
+			var delay=i*0.08;
+
+			var $bounce=$(this).children(".menu-item-bounce");
+
+			TweenMax.fromTo($bounce,0.2,{
+				transformOrigin:"50% 50%"
+			},{
+				delay:delay,
+				scaleX:0.8,
+				scaleY:1.2,
+				force3D:true,
+				ease:Quad.easeInOut,
+				onComplete:function(){
+					TweenMax.to($bounce,0.15,{
+						// scaleX:1.2,
+						scaleY:0.7,
+						force3D:true,
+						ease:Quad.easeInOut,
+						onComplete:function(){
+							TweenMax.to($bounce,3,{
+								// scaleX:1,
+								scaleY:0.8,
+								force3D:true,
+								ease:Elastic.easeOut,
+								easeParams:[1.1,0.12]
+							})
+						}
+					})
+				}
+			});
+
+			TweenMax.to($(this).children(".menu-item-button"),0.5,{
+				delay:delay,
+				y:distance,
+				force3D:true,
+				ease:Quint.easeInOut
+			});
+		})
+	}
+	function closeMenu(){
+		$(".menu-item").each(function(i){
+			var delay=i*0.08;
+
+			var $bounce=$(this).children(".menu-item-bounce");
+
+			TweenMax.fromTo($bounce,0.2,{
+				transformOrigin:"50% 50%"
+			},{
+				delay:delay,
+				scaleX:1,
+				scaleY:0.8,
+				force3D:true,
+				ease:Quad.easeInOut,
+				onComplete:function(){
+					TweenMax.to($bounce,0.15,{
+						// scaleX:1.2,
+						scaleY:1.2,
+						force3D:true,
+						ease:Quad.easeInOut,
+						onComplete:function(){
+							TweenMax.to($bounce,3,{
+								// scaleX:1,
+								scaleY:1,
+								force3D:true,
+								ease:Elastic.easeOut,
+								easeParams:[1.1,0.12]
+							})
+						}
+					})
+				}
+			});
+
+
+			TweenMax.to($(this).children(".menu-item-button"),0.3,{
+				delay:delay,
+				y:0,
+				force3D:true,
+				ease:Quint.easeIn
+			});
+		})
+	}
+
+
+
 
 
 });
