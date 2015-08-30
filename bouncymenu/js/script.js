88880e7dbc0ff0ddbd6f055a2bb4eaa35cccda6b
tamzi <tamrefrank@gmail.com>
2015-08-30 06:52:50 +0300

    ready function for the menu

--- bouncymenu/js/script.js
+++ bouncymenu/js/script.js
@@ -1,1 +1,10 @@
         /*Your Custom Javascript file goes here.*/
+$(document).ready(function(){
+	var menuItemNum=$(".menu-item").length;
+	var angle=360;
+	var distance=80;
+	var startingAngle=180+(-angle/2);
+	var slice=angle/(menuItemNum);
+	TweenMax.globalTimeScale(0.8);
+
+});
