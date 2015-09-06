df50cdc90203ac0dcae7e37e0fc71badb72c6024
tamzi <tamrefrank@gmail.com>
2015-09-06 23:50:24 +0300

    Line Chart function

--- graphProject/js/script.js
+++ graphProject/js/script.js
@@ -1,1 +1,30 @@
         /*Your Custom Javascript file goes here.*/
+// don't try to learn anything from the code, it's a
+// series of hacks. this one's all about the visuals.
+// - @hakimel
+
+var LineChart = function( options ) {
+
+  var data = options.data;
+  var canvas = document.body.appendChild( document.createElement( 'canvas' ) );
+  var context = canvas.getContext( '2d' );
+
+  var rendering = false,
+      paddingX = 40,
+      paddingY = 40,
+      width = options.width || window.innerWidth,
+      height = options.height || window.innerHeight,
+      progress = 0;
+
+  canvas.width = width;
+  canvas.height = height;
+
+  var maxValue,
+      minValue;
+
+  var y1 = paddingY + ( 0.05 * ( height - ( paddingY * 2 ) ) ),
+      y2 = paddingY + ( 0.50 * ( height - ( paddingY * 2 ) ) ),
+      y3 = paddingY + ( 0.95 * ( height - ( paddingY * 2 ) ) );
+
+  format();
+  render();
