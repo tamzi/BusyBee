a55121e3982ab042b977575ff76a51287cfb39be
tamzi <tamrefrank@gmail.com>
2015-09-07 00:09:05 +0300

    canvass init

--- bubbles/js/script.js
+++ bubbles/js/script.js
@@ -1,1 +1,9 @@
         /*Your Custom Javascript file goes here.*/
+var canvas = document.getElementById("canvas");
+var ctx = canvas.getContext("2d");
+var W = canvas.width = window.innerWidth;
+var H = canvas.height = window.innerHeight;
+var particles = [];
+var count = 100;
+
+while ( count-- ) particles.push( new particle() );
