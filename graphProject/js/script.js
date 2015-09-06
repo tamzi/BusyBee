3e62124722ac8e478ab3c17a488bcfa28f50044b
tamzi <tamrefrank@gmail.com>
2015-09-06 23:53:17 +0300

    function render

--- graphProject/js/script.js
+++ graphProject/js/script.js
@@ -51,3 +51,62 @@ function format( force ) {
     } );
 
   }
+
+  function render() {
+
+    if( !rendering ) {
+      requestAnimationFrame( render );
+      return;
+    }
+
+    context.font = '10px sans-serif';
+    context.clearRect( 0, 0, width, height );
+
+    context.fillStyle = '#222';
+    context.fillRect( paddingX, y1, width - ( paddingX * 2 ), 1 );
+    context.fillRect( paddingX, y2, width - ( paddingX * 2 ), 1 );
+    context.fillRect( paddingX, y3, width - ( paddingX * 2 ), 1 );
+
+    if( options.yAxisLabel ) {
+      context.save();
+      context.globalAlpha = progress;
+      context.translate( paddingX - 15, height - paddingY - 10 );
+      context.rotate( -Math.PI / 2 );
+      context.fillStyle = '#fff';
+      context.fillText( options.yAxisLabel, 0, 0 );
+      context.restore();
+    }
+
+    var progressDots = Math.floor( progress * data.length );
+    var progressFragment = ( progress * data.length ) - Math.floor( progress * data.length );
+
+    data.forEach( function( point, i ) {
+      if( i <= progressDots ) {
+        point.x += ( point.targetX - point.x ) * point.speed;
+        point.y += ( point.targetY - point.y ) * point.speed;
+
+        context.save();
+
+        var wordWidth = context.measureText( point.label ).width;
+        context.globalAlpha = i === progressDots ? progressFragment : 1;
+        context.fillStyle = point.future ? '#aaa' : '#fff';
+        context.fillText( point.label, point.x - ( wordWidth / 2 ), height - 18 );
+
+        if( i < progressDots && !point.future ) {
+          context.beginPath();
+          context.arc( point.x, point.y, 4, 0, Math.PI * 2 );
+          context.fillStyle = '#1baee1';
+          context.fill();
+        }
+
+        context.restore();
+      }
+
+    } );
+
+    context.save();
+    context.beginPath();
+    context.strokeStyle = '#1baee1';
+    context.lineWidth = 2;
+
+    var futureStarted = false;
