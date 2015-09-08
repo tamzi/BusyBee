cb3cf2aa3397bf84375eddc70c27f4de8923c02f
tamzi <tamrefrank@gmail.com>
2015-09-08 08:49:00 +0300

    clouds

--- sunriseToSunset/js/script.js
+++ sunriseToSunset/js/script.js
@@ -1,1 +1,11 @@
         /*Your Custom Javascript file goes here.*/
+var clouds = document.getElementById('clouds')
+    sun = document.getElementById('sun')
+    sky = document.getElementById('XMLID_1_')
+    cars = document.getElementById('cars')
+    trees = document.getElementById('trees')
+    travelTime = 1
+    changeTime = 0
+    positionTop = 30
+    positionBottom = 200;
+
