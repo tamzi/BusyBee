231786a5556d55ed6f11280943c3ef5440bec492
tamzi <tamrefrank@gmail.com>
2015-09-11 08:59:49 +0300

    if browser cna handle the slidesThingy

--- SlidesThingy/js/script.js
+++ SlidesThingy/js/script.js
@@ -3,4 +3,18 @@ window.slides = function( container ) {
 
 	// Dispatched when the current layer changes
 	var changed = new slides.Signal();
+	// All layers in this instance of slides
+	var layers = Array.prototype.slice.call( container.querySelectorAll( '.layer' ) );
+
+	// Flag if the browser is capable of handling our fancy transition
+	var capable =	'WebkitPerspective' in document.body.style ||
+					'MozPerspective' in document.body.style ||
+					'msPerspective' in document.body.style ||
+					'OPerspective' in document.body.style ||
+					'perspective' in document.body.style;
+
+	if( capable ) {
+		container.classList.add( 'capable' );
+	}
+
 
