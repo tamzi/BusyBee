e1f2fc87147fff2f7dcfcdc33ec0621c12c58b42
tamzi <tamrefrank@gmail.com>
2015-09-14 14:49:01 +0300

    adding a filter method and staging the page

--- newsWallSort/js/script.js
+++ newsWallSort/js/script.js
@@ -1,6 +1,16 @@
         /*Your Custom Javascript file goes here.*/
+			/*Adding the sorting js*/
+var $orgPage = $('.page').clone();
 
-/*
-NO JS...just lists
-*/
-s
+/**
+ * Filters at most 3 random tiles
+ */
+function filter() {
+  var $tiles = $('.tile'),
+    numTiles = $tiles.length,
+    rand = -1,
+    filtered = Math.floor(Math.random() * (numTiles - 1)) + 1,
+    i;
+
+  $tiles.find('.live').data('filtered', false);
+
