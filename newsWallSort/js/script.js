0b1e62a4e2cfb121f808cdbc291622dd2f32b232
tamzi <tamrefrank@gmail.com>
2015-09-14 14:51:03 +0300

    filter random tiles

--- newsWallSort/js/script.js
+++ newsWallSort/js/script.js
@@ -14,3 +14,31 @@ function filter() {
 
   $tiles.find('.live').data('filtered', false);
 
+  // Filter out some random tiles
+  for (i = 0; i < filtered; i++) {
+    rand = Math.floor(Math.random() * numTiles) + 1;
+
+    if ($('.tile:nth-child(' + rand + ')').data('filtered')) {
+      continue;
+    }
+
+    $('.tile:nth-child(' + rand + ')')
+      .find('.live')
+      .animate({
+        opacity: 0
+      }, {
+        duration: 500,
+        easing: 'easeOutExpo',
+        complete: function() {
+          $(this).find('.live').css({
+            top: 120
+          });
+        }
+      })
+      .data('filtered', true);
+  }
+
+  setTimeout(function() {
+    repositionTiles();
+  }, 100);
+}
