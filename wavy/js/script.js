33d120312ce212da1512b39d7c0419b529275341
tamzi <tamrefrank@gmail.com>
2015-09-19 02:43:11 +0300

    added interpolation

--- wavy/js/script.js
+++ wavy/js/script.js
@@ -132,3 +132,17 @@ function Spline() {
     return v2;
   }
 
+   // Catmull-Rom
+
+  function interpolate(p0, p1, p2, p3, t) {
+
+    var v0 = (p2 - p0) * 0.5;
+    var v1 = (p3 - p1) * 0.5;
+    var t2 = t * t;
+    var t3 = t * t2;
+    return (2 * p1 - 2 * p2 + v0 + v1) * t3 + (-3 * p1 + 3 * p2 - 2 * v0 - v1) * t2 + v0 * t + p1;
+
+  }
+
+}
+
