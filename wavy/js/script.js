428a258f440a5d6ec7a6e7034f0bb18a3fe5156b
tamzi <tamrefrank@gmail.com>
2015-09-19 02:50:24 +0300

    FXN GRAD

--- wavy/js/script.js
+++ wavy/js/script.js
@@ -165,3 +165,12 @@ function Spline() {
     p[256 + i] = p[i];
 
   }
+function grad(hash, x, y, z) {
+
+    var h = hash & 15;
+    var u = h < 8 ? x : y,
+      v = h < 4 ? y : h == 12 || h == 14 ? x : z;
+    return ((h & 1) == 0 ? u : -u) + ((h & 2) == 0 ? v : -v);
+
+  }
+
