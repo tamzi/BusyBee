5e9f7bf9b90e79e0f8e8d878dee7471061cbc5ae
tamzi <tamrefrank@gmail.com>
2015-09-19 02:42:07 +0300

    fxn spline

--- wavy/js/script.js
+++ wavy/js/script.js
@@ -106,3 +106,29 @@ function draw() {
 
 }
 
+function Spline() {
+
+  var c = [],
+    v2 = {
+      x: 0,
+      y: 0
+    },
+    point, intPoint, weight;
+
+  this.get2DPoint = function(points, k) {
+
+    point = (points.length - 1) * k;
+    intPoint = Math.floor(point);
+    weight = point - intPoint;
+
+    c[0] = intPoint == 0 ? intPoint : intPoint - 1;
+    c[1] = intPoint;
+    c[2] = intPoint > points.length - 2 ? intPoint : intPoint + 1;
+    c[3] = intPoint > points.length - 3 ? intPoint : intPoint + 2;
+
+    v2.x = interpolate(points[c[0]].x, points[c[1]].x, points[c[2]].x, points[c[3]].x, weight);
+    v2.y = interpolate(points[c[0]].y, points[c[1]].y, points[c[2]].y, points[c[3]].y, weight);
+
+    return v2;
+  }
+
