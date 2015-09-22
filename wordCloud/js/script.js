d38b9dfeaf106465d4140099babd4a025d7c7b05
tamzi <tamrefrank@gmail.com>
2015-09-23 02:45:57 +0300

    intializing js

--- wordCloud/js/script.js
+++ wordCloud/js/script.js
@@ -1,1 +1,14 @@
         /*Your Custom Javascript file goes here.*/
+$(document).ready(function() {
+  if (!$('#myCanvas').tagcanvas({
+      //textColour: '#ff0000',
+      outlineColour: '#ff00ff',
+      reverse: true,
+      depth: 0.8,
+      maxSpeed: 0.05,
+      textFont: null,
+      textColour: null,
+      weightMode: 'both',
+      weight: true,
+     },
+});
