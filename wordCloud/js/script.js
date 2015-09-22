eac34943220fca8ce6953fbb763c25d1fbe1ce9f
tamzi <tamrefrank@gmail.com>
2015-09-23 02:55:01 +0300

    final script commit

--- wordCloud/js/script.js
+++ wordCloud/js/script.js
@@ -1,18 +1,24 @@
         /*Your Custom Javascript file goes here.*/
 
-$(document).ready(function () {
-	if (!$('#myCanvas').tagcanvas({
-		//textColour: '#ff0000',
-		outlineColour: '#ff00ff',
-		reverse: true,
-		depth: 0.8,
-		maxSpeed: 0.05,
-		textFont: null,
-		textColour: null,
-		weightMode: 'both',
-		weight: true, 'tags')) {
-     // something went wrong, hide the canvas container
+$(document).ready(function() {
+  if (!$('#myCanvas').tagcanvas({
+      //textColour: '#ff0000',
+      outlineColour: '#ff00ff',
+      reverse: true,
+      depth: 0.8,
+      maxSpeed: 0.05,
+      textFont: null,
+      textColour: null,
+      weightMode: 'both',
+      weight: true,
+      weightGradient: {
+        0: '#f00', // red
+        //0.33: '#ff0', // yellow
+        //0.66: '#0f0', // green
+        1: '#00f' // blue
+      }
+    }, 'tags')) {
+    // something went wrong, hide the canvas container
     $('#myCanvasContainer').hide();
   }
-	},
 });
