8c8bc8aaf7794a008b65c51f2741966820c8ee04
tamzi <tamrefrank@gmail.com>
2015-09-23 02:51:33 +0300

    correcting lint errors

--- wordCloud/js/script.js
+++ wordCloud/js/script.js
@@ -1,14 +1,15 @@
         /*Your Custom Javascript file goes here.*/
-$(document).ready(function() {
-  if (!$('#myCanvas').tagcanvas({
-      //textColour: '#ff0000',
-      outlineColour: '#ff00ff',
-      reverse: true,
-      depth: 0.8,
-      maxSpeed: 0.05,
-      textFont: null,
-      textColour: null,
-      weightMode: 'both',
-      weight: true,
-     },
+
+$(document).ready(function () {
+	if (!$('#myCanvas').tagcanvas({
+		//textColour: '#ff0000',
+		outlineColour: '#ff00ff',
+		reverse: true,
+		depth: 0.8,
+		maxSpeed: 0.05,
+		textFont: null,
+		textColour: null,
+		weightMode: 'both',
+		weight: true,
+	},
 });
