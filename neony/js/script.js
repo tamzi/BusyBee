2093675a14bc8c007ae4d6cdc4308187ce53bc9a
tamzi <tamrefrank@gmail.com>
2015-09-25 00:01:29 +0300

    creating the switch to animate fxn

--- neony/js/script.js
+++ neony/js/script.js
@@ -1,1 +1,6 @@
         /*Your Custom Javascript file goes here.*/
+var tswitch = document.getElementById('switch');
+tswitch.onclick = function() {
+       this.style.display = 'none';
+       document.getElementById('lights').className = 'animate';
+};
