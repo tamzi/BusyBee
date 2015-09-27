a72ad91db1b1acb895e15055250b434de1ba1c62
tamzi <tamrefrank@gmail.com>
2015-09-27 22:14:33 +0300

    creating mountain peaks

--- landscape/js/script.js
+++ landscape/js/script.js
@@ -95,4 +95,69 @@ function animate() {
   mtn.draw();
 }
 
+function Mountains(peaks, seed) {
+  var points = [];
+  this.init = function() {
+    var step = w / peaks,
+      y = 0;
+
+    points.push({
+      x: 0,
+      y: y
+    });
+    for (var i = 1; i <= peaks; i++) {
+      y = y + (Math.random() * 20) - 10;
+      points.push({
+        x: i * step,
+        y: y
+      });
+    }
+  };
+  this.draw = function() {
+    c.save();
+    //c.fillStyle = "rgba(20,20,20,1)";
+    c.fillStyle = newGradient({
+      type: "linear",
+      x1: 0,
+      y1: 0,
+      x2: 0,
+      y2: h,
+      stops: [{
+        s: 1,
+        c: "rgba(0,0,0,1)"
+      }, {
+        s: 0,
+        c: "rgba(80,80,80,1)"
+      }]
+    });
+    c.beginPath();
+    c.moveTo(points[0].x, h / 2 - points[0].y);
+    for (var p = 1; p < points.length; p++) {
+      c.lineTo(points[p].x, h / 2 - points[p].y);
+    }
+    c.lineTo(w, h);
+    c.lineTo(0, h);
+    c.closePath();
+    c.fill();
+    c.restore();
+
+    c.globalCompositeOperation = "lighter";
+    c.fillStyle = "rgba(" + (h - mY) + "," + (h - mY) + "," + (h - mY) + ",0.03)";
+    for (var p = 0; p < points.length - 1; p++) {
+      var va1 = Math.atan2(h / 2 - points[p].y - per.y, points[p].x - per.x),
+        va2 = Math.atan2(h / 2 - points[p + 1].y - per.y, points[p + 1].x - per.x);
+
+      c.beginPath();
+      c.moveTo(points[p].x, h / 2 - points[p].y);
+      c.lineTo(points[p + 1].x, h / 2 - points[p + 1].y);
+      c.lineTo(points[p + 1].x + Math.cos(va2) * w, h / 2 - points[p + 1].y + Math.sin(va2) * w);
+      c.lineTo(points[p].x + Math.cos(va1) * w, h / 2 - points[p].y + Math.sin(va1) * w);
+      c.closePath();
+      c.fill();
+    }
+  };
+
+  this.init();
+
+}
 
