0e94e10d45a1a9df7fe240ad99d877fbe4934095
tamzi <tamrefrank@gmail.com>
2015-09-27 22:09:06 +0300

    staging js files

--- landscape/js/script.js
+++ landscape/js/script.js
@@ -1,1 +1,12 @@
         /*Your Custom Javascript file goes here.*/
+var canvas, c, w, h,
+  twoPI = Math.PI * 2,
+  mX, mY,
+  resize = true,
+  mousemove = true,
+  per = {
+    x: 0,
+    y: 0
+  },
+  mtn, trackmouse = false;
+
