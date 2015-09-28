b38f5d57c568e2ea7405af295c8f15350f56abf1
tamzi <tamrefrank@gmail.com>
2015-09-28 05:40:16 +0300

    request animation frame

--- lightning/js/script.js
+++ lightning/js/script.js
@@ -201,5 +201,17 @@ var setupRAF = function() {
     window.cancelAnimationFrame = window[vendors[x] + 'CancelAnimationFrame'] || window[vendors[x] + 'CancelRequestAnimationFrame'];
   };
 
+  if (!window.requestAnimationFrame) {
+    window.requestAnimationFrame = function(callback, element) {
+      var currTime = new Date().getTime();
+      var timeToCall = Math.max(0, 16 - (currTime - lastTime));
+      var id = window.setTimeout(function() {
+        callback(currTime + timeToCall);
+      }, timeToCall);
+      lastTime = currTime + timeToCall;
+      return id;
+    };
+  };
+
 
 };
