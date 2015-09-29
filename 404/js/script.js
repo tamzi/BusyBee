9f92b2d22725e75b1c947231f14be0ca8282ba6a
tamzi <tamrefrank@gmail.com>
2015-09-29 23:56:38 +0300

    initilizing fx particles

--- 404/js/script.js
+++ 404/js/script.js
@@ -1,1 +1,28 @@
         /*Your Custom Javascript file goes here.*/
+
+// js source http://codepen.io/moklick/pen/zKleC
+
+var Application = ( function () {
+        var canvas;
+        var ctx;
+        var imgData;
+        var pix;
+        var WIDTH;
+        var HEIGHT;
+        var flickerInterval;
+
+        var init = function () {
+            canvas = document.getElementById('canvas');
+            ctx = canvas.getContext('2d');
+            canvas.width = WIDTH = 700;
+            canvas.height = HEIGHT = 500;
+            ctx.fillStyle = 'white';
+            ctx.fillRect(0, 0, WIDTH, HEIGHT);
+            ctx.fill();
+            imgData = ctx.getImageData(0, 0, WIDTH, HEIGHT);
+            pix = imgData.data;
+            flickerInterval = setInterval(flickering, 30);
+        };
+
+
+    }());
