7a8ebf821e5d54c870660ac38219a9b864ba0d83
tamzi <tamrefrank@gmail.com>
2015-10-01 05:40:03 +0300

    css on js file...no wonder not working!!! Need to take a nap if this is happening!

--- wiggle/js/script.js
+++ wiggle/js/script.js
@@ -1,22 +1,2 @@
         /*Your Custom Javascript file goes here.*/
 
-@keyframes wiggle {
-  0% {
-    transform: rotate(-10deg);
-  }
-  100% {
-    transform: rotate(10deg);
-  }
-}
-div {
-  position: absolute;
-  left: 50%;
-  top: 50%;
-}
-
-b {
-  position: absolute;
-  display: block;
-  border-radius: 5%;
-  transform: rotate(-10deg);
-}
