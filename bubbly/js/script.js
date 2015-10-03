c9d1194b8d8e47bef6fe1a5ed6fe3292c31017ee
tamzi <tamrefrank@gmail.com>
2015-10-03 05:09:17 +0300

    JUST BEFORE image close

--- bubbly/js/script.js
+++ bubbly/js/script.js
@@ -504,9 +504,129 @@ function stackBlurCanvasRGB(canvas, top_x, top_y, width, height, radius) {
     }
 
 
+    stackIn = stackStart;
+    stackOut = stackEnd;
+    for (x = 0; x < width; x++) {
+      pixels[yi] = (r_sum * mul_sum) >> shg_sum;
+      pixels[yi + 1] = (g_sum * mul_sum) >> shg_sum;
+      pixels[yi + 2] = (b_sum * mul_sum) >> shg_sum;
 
+      r_sum -= r_out_sum;
+      g_sum -= g_out_sum;
+      b_sum -= b_out_sum;
 
+      r_out_sum -= stackIn.r;
+      g_out_sum -= stackIn.g;
+      b_out_sum -= stackIn.b;
 
+      p = (yw + ((p = x + radius + 1) < widthMinus1 ? p : widthMinus1)) << 2;
 
+      r_in_sum += (stackIn.r = pixels[p]);
+      g_in_sum += (stackIn.g = pixels[p + 1]);
+      b_in_sum += (stackIn.b = pixels[p + 2]);
 
+      r_sum += r_in_sum;
+      g_sum += g_in_sum;
+      b_sum += b_in_sum;
+
+      stackIn = stackIn.next;
+
+      r_out_sum += (pr = stackOut.r);
+      g_out_sum += (pg = stackOut.g);
+      b_out_sum += (pb = stackOut.b);
+
+      r_in_sum -= pr;
+      g_in_sum -= pg;
+      b_in_sum -= pb;
+
+      stackOut = stackOut.next;
+
+      yi += 4;
+    }
+    yw += width;
+  }
+
+  for (x = 0; x < width; x++) {
+    g_in_sum = b_in_sum = r_in_sum = g_sum = b_sum = r_sum = 0;
+
+    yi = x << 2;
+    r_out_sum = radiusPlus1 * (pr = pixels[yi]);
+    g_out_sum = radiusPlus1 * (pg = pixels[yi + 1]);
+    b_out_sum = radiusPlus1 * (pb = pixels[yi + 2]);
+
+    r_sum += sumFactor * pr;
+    g_sum += sumFactor * pg;
+    b_sum += sumFactor * pb;
+
+    stack = stackStart;
+
+    for (i = 0; i < radiusPlus1; i++) {
+      stack.r = pr;
+      stack.g = pg;
+      stack.b = pb;
+      stack = stack.next;
+    }
+
+    yp = width;
+
+    for (i = 1; i <= radius; i++) {
+      yi = (yp + x) << 2;
+
+      r_sum += (stack.r = (pr = pixels[yi])) * (rbs = radiusPlus1 - i);
+      g_sum += (stack.g = (pg = pixels[yi + 1])) * rbs;
+      b_sum += (stack.b = (pb = pixels[yi + 2])) * rbs;
+
+      r_in_sum += pr;
+      g_in_sum += pg;
+      b_in_sum += pb;
+
+      stack = stack.next;
+
+      if (i < heightMinus1) {
+        yp += width;
+      }
+    }
+
+    yi = x;
+    stackIn = stackStart;
+    stackOut = stackEnd;
+    for (y = 0; y < height; y++) {
+      p = yi << 2;
+      pixels[p] = (r_sum * mul_sum) >> shg_sum;
+      pixels[p + 1] = (g_sum * mul_sum) >> shg_sum;
+      pixels[p + 2] = (b_sum * mul_sum) >> shg_sum;
+
+      r_sum -= r_out_sum;
+      g_sum -= g_out_sum;
+      b_sum -= b_out_sum;
+
+      r_out_sum -= stackIn.r;
+      g_out_sum -= stackIn.g;
+      b_out_sum -= stackIn.b;
+
+      p = (x + (((p = y + radiusPlus1) < heightMinus1 ? p : heightMinus1) * width)) << 2;
+
+      r_sum += (r_in_sum += (stackIn.r = pixels[p]));
+      g_sum += (g_in_sum += (stackIn.g = pixels[p + 1]));
+      b_sum += (b_in_sum += (stackIn.b = pixels[p + 2]));
+
+      stackIn = stackIn.next;
+
+      r_out_sum += (pr = stackOut.r);
+      g_out_sum += (pg = stackOut.g);
+      b_out_sum += (pb = stackOut.b);
+
+      r_in_sum -= pr;
+      g_in_sum -= pg;
+      b_in_sum -= pb;
+
+      stackOut = stackOut.next;
+
+      yi += width;
+    }
+  }
+
+  context.putImageData(imageData, top_x, top_y);
+
+}
 
