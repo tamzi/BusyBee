65d2fa87978cf692cd285f7f2ae9ea442b4518d1
tamzi <tamrefrank@gmail.com>
2015-10-03 04:45:52 +0300

    count the js iles and init the properties

--- bubbly/README.md
+++ bubbly/README.md
@@ -2,4 +2,4 @@
 
 White bubbles from the top,
 
-THis can be used as the backkground of a header and any part of a div when working on a html project
+This can be used as the backkground of a header and any part of a div when working on a html project
--- bubbly/js/script.js
+++ bubbly/js/script.js
@@ -1,1 +1,23 @@
         /*Your Custom Javascript file goes here.*/
+
+
+window.onload = function() {
+  var count = 200;
+  var width = window.innerWidth;
+  var height = window.innerHeight;
+  var flakes = {};
+  var winds = {};
+  var elm = document.createElement('canvas');
+  var pixelDensity = window.devicePixelRatio || 1;
+
+  elm.setAttribute('id', "snow");
+  elm.setAttribute('width', width * pixelDensity);
+  elm.setAttribute('height', height * pixelDensity);
+
+  elm.style.width = width + "px";
+  elm.style.height = height + "px";
+
+  var ctx = elm.getContext('2d');
+  ctx.globalCompositeOperation = 'xor';
+  document.body.appendChild(elm);
+
