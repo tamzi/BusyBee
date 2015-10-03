a7b9da5220c1bb6634790e49f82bb68e8122c61a
tamzi <tamrefrank@gmail.com>
2015-10-03 05:09:41 +0300

    all the dots are stacked

--- bubbly/js/script.js
+++ bubbly/js/script.js
@@ -630,3 +630,11 @@ function stackBlurCanvasRGB(canvas, top_x, top_y, width, height, radius) {
 
 }
 
+function BlurStack() {
+  this.r = 0;
+  this.g = 0;
+  this.b = 0;
+  this.a = 0;
+  this.next = null;
+}
+
