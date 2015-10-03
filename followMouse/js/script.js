9f0eb8fb590a69be88e7d8df6cdc36e3e5da887a
tamzi <tamrefrank@gmail.com>
2015-10-03 04:25:59 +0300

    var particle and particle count

--- followMouse/js/script.js
+++ followMouse/js/script.js
@@ -1,5 +1,25 @@
         /*Your Custom Javascript file goes here.*/
 
 (function () {
+	var Particle, particleCount, particles, sketch, z;
+    sketch = Sketch.create();
+    particles = [];
+    particleCount = 750;
+    sketch.mouse.x = sketch.width / 2;
+    sketch.mouse.y = sketch.height / 2;
+    sketch.strokeStyle = 'hsla(200, 50%, 50%, .4)';
+    sketch.globalCompositeOperation = 'lighter';
+    Particle = function () {
+        this.x = random(sketch.width);
+        this.y = random(sketch.height, sketch.height * 2);
+        this.vx = 0;
+        this.vy = -random(1, 10) / 5;
+        this.radius = this.baseRadius = 1;
+        this.maxRadius = 50;
+        this.threshold = 150;
+        return this.hue = random(180, 240);
+    };
+
+
 
 }.call(this));
