e7448e16fcc8ca9e91e0fa3235b03ac731e70e2d
tamzi <tamrefrank@gmail.com>
2015-10-03 04:49:40 +0300

    blur image

--- bubbly/js/script.js
+++ bubbly/js/script.js
@@ -138,4 +138,30 @@ var shg_table = [
   24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24, 24
 ];
 
+function stackBlurImage(imageID, canvasID, radius, blurAlphaChannel) {
+
+  var img = document.getElementById(imageID);
+  var w = img.naturalWidth;
+  var h = img.naturalHeight;
+
+  var canvas = document.getElementById(canvasID);
+
+  canvas.style.width = w + "px";
+  canvas.style.height = h + "px";
+  canvas.width = w;
+  canvas.height = h;
+
+  var context = canvas.getContext("2d");
+  context.clearRect(0, 0, w, h);
+  context.drawImage(img, 0, 0);
+
+  if (isNaN(radius) || radius < 1) return;
+
+  if (blurAlphaChannel)
+    stackBlurCanvasRGBA(canvasID, 0, 0, w, h, radius);
+  else
+    stackBlurCanvasRGB(canvasID, 0, 0, w, h, radius);
+}
+
+
 
