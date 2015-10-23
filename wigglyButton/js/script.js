5c2e63c4065516bac2fd7341bc928dc76328830d
tamzi <tamrefrank@gmail.com>
2015-10-24 00:38:08 +0300

    creating a circle

--- wigglyButton/js/script.js
+++ wigglyButton/js/script.js
@@ -1,1 +1,10 @@
         /*Your Custom Javascript file goes here.*/
+
+'use strict';
+var myCircle = document.getElementById('example');
+var animations = [
+    'click me',
+    'pulse',
+    'bounce',
+    'wiggle'
+];
