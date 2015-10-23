d5fb8ad38db348b29c3b3eca5b623d4cbd0a9df6
tamzi <tamrefrank@gmail.com>
2015-10-24 00:38:24 +0300

    added circle

--- wigglyButton/js/script.js
+++ wigglyButton/js/script.js
@@ -8,3 +8,12 @@ var animations = [
     'bounce',
     'wiggle'
 ];
+var currentAnimation = 0;
+myCircle.addEventListener('click', function () {
+    currentAnimation += 1;
+    if (currentAnimation > animations.length - 1) {
+        currentAnimation = 0;
+    }
+    myCircle.className = 'circle ' + animations[currentAnimation];
+    myCircle.innerHTML = animations[currentAnimation];
+});
