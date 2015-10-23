e5afb874c195e22c8bc2d6387e8bd1371d4ee4fc
tamzi <tamrefrank@gmail.com>
2015-10-23 23:07:09 +0300

    added js file function fir parallax mouse movement

--- parallaxCard/js/script.js
+++ parallaxCard/js/script.js
@@ -1,1 +1,10 @@
         /*Your Custom Javascript file goes here.*/
+
+
+//simple jQuery Parallax Mouse Movement
+// by Hugh Balboa
+$('#Parallax-content').mousemove(function (e) {
+   var amountMovedX = (e.pageX * -0.3 / 6);
+   var amountMovedY = (e.pageY * -1.29 / 6);
+   $(this).css('background-position', amountMovedX + 'px ' + amountMovedY + 'px ');
+});
