f44b17ecaa0c52886f495b2f713de84b5df21730
tamzi <tamrefrank@gmail.com>
2015-10-25 17:19:50 +0300

    preLoad images

--- happyNewYear/js/script.js
+++ happyNewYear/js/script.js
@@ -6,4 +6,17 @@ function init() {
   var animationOn = false;
 
 
+
+
+
+//preload images
+var glass = new Image()
+glass.src = "https://s3-us-west-2.amazonaws.com/s.cdpn.io/127738/glass-01.png";
+var cross = new Image()
+cross.src = "https://s3-us-west-2.amazonaws.com/s.cdpn.io/127738/cross.png";
+
+window.onload = function() {
+  init();
+
+
 };
