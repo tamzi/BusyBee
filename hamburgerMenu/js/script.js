57e1b81151b3e10a42313a5ce7639a53f1f95e98
tamzi <tamrefrank@gmail.com>
2015-11-14 21:01:04 +0300

    menu active onClick

--- hamburgerMenu/js/script.js
+++ hamburgerMenu/js/script.js
@@ -9,5 +9,18 @@ $(document).ready(function() {
     $demo.addClass("menu-active");
   });
 
+$(document).on("click", ".demo__menu-item", function() {
+    var $item = $(this);
+    var targetSection = $item.data("section");
+    $item.addClass("clicked");
+    $demo.removeClass("menu-active");
+    $(".demo__section.active-section").removeClass("active-section");
+    $(".demo__section--" + targetSection).addClass("active-section");
+
+    setTimeout(function() {
+      $item.removeClass("clicked");
+    }, menuTextAT);
+  });
+
 
 });
