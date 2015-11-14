8497e6cd754972690560ee0f10f251a0058e0198
tamzi <tamrefrank@gmail.com>
2015-11-14 21:00:14 +0300

    init demo and onClick

--- hamburgerMenu/js/script.js
+++ hamburgerMenu/js/script.js
@@ -1,1 +1,13 @@
         /*Your Custom Javascript file goes here.*/
+
+$(document).ready(function() {
+
+  var $demo = $(".demo");
+  var menuTextAT = 500;
+
+  $(document).on("click", ".demo__menu-btn", function() {
+    $demo.addClass("menu-active");
+  });
+
+
+});
